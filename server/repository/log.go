e8899a01a9601f95a862b2fa42247b4dd487d7b4
logica <84759675+logica0419@users.noreply.github.com>
2022-01-24 04:59:12 +0900

    implemented log repository

--- server/repository/log.go
+++ server/repository/log.go
@@ -19,3 +19,23 @@ type Log struct {
 func (Log) TableName() string {
 	return "benchmark_log"
 }
+
+func (repo *Repository) SelectLogsByUserID(userID uuid.UUID) ([]*Log, error) {
+	var logs []*Log
+
+	res := repo.getTx().Joins("User").Joins("Log").Where("user_id = ?", userID).Find(&logs)
+	if res.Error != nil {
+		return nil, res.Error
+	}
+
+	return logs, nil
+}
+
+func (repo *Repository) InsertLog(log Log) error {
+	res := repo.getTx().Create(&log)
+	if res.Error != nil {
+		return res.Error
+	}
+
+	return nil
+}
