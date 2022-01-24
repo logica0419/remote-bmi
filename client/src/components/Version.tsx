ee60ad53325f60a7cbc69aef25b1edd629398970
logica <84759675+logica0419@users.noreply.github.com>
2022-01-24 23:09:04 +0900

    fix styles

--- client/src/components/Version.tsx
+++ client/src/components/Version.tsx
@@ -1,5 +1,15 @@
+import { css } from "@emotion/react";
 import axios from "axios";
 import { useEffect, useState, VFC } from "react";
+
+const styles = {
+  container: css`
+    display: flex;
+  `,
+  bold: css`
+    font-weight: bold;
+  `,
+};
 
 const VersionDisplay: VFC = () => {
   const [version, setVersion] = useState("loading...");
@@ -16,7 +26,12 @@ const VersionDisplay: VFC = () => {
       });
   }, []);
 
-  return <>ISUCON version: {version}</>;
+  return (
+    <div css={styles.container}>
+      ISUCON version:&nbsp;
+      <div css={styles.bold}>{version}</div>
+    </div>
+  );
 };
 
 export default VersionDisplay;
--- client/src/style.tsx
+++ client/src/style.tsx
@@ -8,9 +8,8 @@ const GlobalStyle: VFC = () => {
         body {
           background-color: #ffffff;
           margin: 0;
-          font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto",
-            "Oxygen", "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans",
-            "Helvetica Neue", sans-serif;
+          font-family: -apple-system, BlinkMacSystemFont, "Roboto", "Oxygen",
+            "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue";
           -webkit-font-smoothing: antialiased;
           -moz-osx-font-smoothing: grayscale;
         }
