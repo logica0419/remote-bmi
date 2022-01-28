eca7a21e22adb32055275528a291aeca6b78e74c
logica <84759675+logica0419@users.noreply.github.com>
2022-01-29 08:22:43 +0900

    implemented time parser

---
+++ client/src/utils/time.ts
@@ -0,0 +1,15 @@
+export const TimeToStr = (date: Date) => {
+  date = new Date(date);
+
+  return (
+    ("0" + (date.getMonth() + 1)).slice(-2) +
+    "/" +
+    ("0" + date.getDate()).slice(-2) +
+    " " +
+    ("0" + date.getHours()).slice(-2) +
+    ":" +
+    ("0" + date.getMinutes()).slice(-2) +
+    ":" +
+    ("0" + date.getSeconds()).slice(-2)
+  );
+};
