1b8344d5445156c45367fc834accca3eac79c887
logica <84759675+logica0419@users.noreply.github.com>
2022-01-29 08:31:06 +0900

    implemented log display

---
+++ client/src/components/Home/Log/LogList.tsx
@@ -0,0 +1,91 @@
+import { css } from "@emotion/react";
+import { Dispatch, FormEvent, SetStateAction, VFC } from "react";
+import { useSelector } from "react-redux";
+import { RootState } from "../../../store";
+import { TimeToStr } from "../../../utils/time";
+import { Log } from "../../../utils/types";
+
+const styles = {
+  container: css`
+    max-height: min(25vh, 30vw);
+    overflow-y: auto;
+  `,
+  table: css`
+    table-layout: fixed;
+    border-collapse: collapse;
+  `,
+  th: css`
+    padding: 0 10px;
+    border: solid 2px;
+  `,
+  td: css`
+    padding: 0 10px;
+    border: solid 1px;
+  `,
+  button: (color: string) => {
+    return css`
+      font-size: min(20px, calc(2vw));
+      border: 0;
+      border-radius: 0.4em;
+      padding: 0.2em 0.4em;
+      background: ${color};
+      :hover {
+        opacity: 0.6;
+        cursor: pointer;
+      }
+    `;
+  },
+};
+
+interface Props {
+  setSelectedLog: Dispatch<SetStateAction<Log | undefined>>;
+}
+
+const LogList: VFC<Props> = ({ setSelectedLog }) => {
+  const logs = useSelector((state: RootState) => state.logs);
+
+  const onSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const target = e.currentTarget;
+    const id = target.name;
+
+    const log = logs.find((log) => log.id === id);
+    setSelectedLog(log);
+  };
+
+  return (
+    <div css={styles.container}>
+      <table css={styles.table}>
+        <thead>
+          <tr>
+            <th css={styles.th}>Time</th>
+            <th css={styles.th}>Server</th>
+            <th css={styles.th}>Log</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          {logs.map((log) => {
+            return (
+              <tr key={log.id}>
+                <td css={styles.td}>{TimeToStr(log.created_at)}</td>
+                <td css={styles.td}>{log.server_number}</td>
+                <td css={styles.td}>
+                  <form name={log.id} onSubmit={onSubmit}>
+                    <input
+                      css={styles.button("#e0e0e0")}
+                      type="submit"
+                      value="See Log"
+                    />
+                  </form>
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default LogList;
---
+++ client/src/components/Home/Log/index.tsx
@@ -0,0 +1,80 @@
+import { css } from "@emotion/react";
+import axios from "axios";
+import { useEffect, useState, VFC } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { AppDispatch, RootState } from "../../../store";
+import { registerLogs } from "../../../store/logs";
+import { TimeToStr } from "../../../utils/time";
+import { Log } from "../../../utils/types";
+import LogList from "./LogList";
+
+const styles = {
+  container: css`
+    margin-top: 1em;
+    align-items: center;
+  `,
+  title: css`
+    line-height: min(10px, calc(2vw - 10px));
+  `,
+  codeBlock: css`
+    margin: 1em 0;
+    padding: 1em;
+    border-radius: 5px;
+    background: #25292f;
+    color: #ffffff;
+    white-space: pre-wrap;
+    font-family: source-code-pro, Menlo, Monaco, Consolas, "Courier New",
+      monospace;
+    font-size: 15px;
+    text-align: left;
+  `,
+  button: (color: string) => {
+    return css`
+      margin: 0.5em 0.2em 0;
+      font-size: min(25px, calc(5px + 2vw));
+      border: 0;
+      border-radius: 0.4em;
+      padding: 0.1em 0.3em;
+      background: ${color};
+      :hover {
+        opacity: 0.6;
+        cursor: pointer;
+      }
+    `;
+  },
+};
+
+const Logs: VFC = () => {
+  const [selectedLog, setSelectedLog] = useState<Log>();
+
+  const logs = useSelector((state: RootState) => state.logs);
+  const dispatch = useDispatch<AppDispatch>();
+
+  useEffect(() => {
+    axios
+      .get<Log[]>("/api/logs")
+      .then(({ data }) => {
+        dispatch(registerLogs(data));
+      })
+      .catch(() => {
+        alert("ログの取得に失敗しました");
+      });
+  }, []);
+
+  return (
+    <div css={styles.container}>
+      <h2 css={styles.title}>Logs</h2>
+      {logs.length ? <LogList setSelectedLog={setSelectedLog} /> : "No Logs"}
+      {selectedLog && (
+        <>
+          <h3 css={styles.title}>Selected Log</h3>
+          Time: {TimeToStr(selectedLog.created_at)}
+          <br />
+          <pre css={styles.codeBlock}>{selectedLog.stdout}</pre>
+        </>
+      )}
+    </div>
+  );
+};
+
+export default Logs;
