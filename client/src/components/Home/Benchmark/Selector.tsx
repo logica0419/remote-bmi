0deba5b468c3c36e9c34d18fb5736e7521f95ac5
logica <84759675+logica0419@users.noreply.github.com>
2022-01-29 04:54:52 +0900

    created server selector

---
+++ client/src/components/Home/Benchmark/Selector.tsx
@@ -0,0 +1,34 @@
+import { css } from "@emotion/react";
+import { Dispatch, SetStateAction, VFC } from "react";
+import { useSelector } from "react-redux";
+import { RootState } from "../../../store";
+
+const styles = {
+  selector: css`
+    font-size: min(25px, calc(5px + 2vw));
+    border-radius: 0.5em;
+  `,
+};
+
+interface Props {
+  serverNumber: number;
+  setServerNumber: Dispatch<SetStateAction<number>>;
+}
+
+const Selector: VFC<Props> = ({ serverNumber, setServerNumber }) => {
+  const servers = useSelector((state: RootState) => state.servers);
+
+  const onChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    setServerNumber(Number(e.target.value));
+  };
+
+  return (
+    <select css={styles.selector} value={serverNumber} onChange={onChange}>
+      {servers.map((server) => (
+        <option value={server.server_number}>{server.server_number}</option>
+      ))}
+    </select>
+  );
+};
+
+export default Selector;
