f41b959ee30e0c1cce2811e3a78934bb08218d58
logica <84759675+logica0419@users.noreply.github.com>
2022-01-29 04:54:21 +0900

    fixed time bug

--- server/main.go
+++ server/main.go
@@ -1,6 +1,21 @@
 package main
 
-import "github.com/logica0419/remote-bmi/server/cmd"
+import (
+	"time"
+
+	"github.com/logica0419/remote-bmi/server/cmd"
+)
+
+func init() {
+	const location = "Asia/Tokyo"
+
+	loc, err := time.LoadLocation(location)
+	if err != nil {
+		loc = time.FixedZone(location, 9*60*60)
+	}
+
+	time.Local = loc
+}
 
 func main() {
 	cmd.Execute()
--- server/repository/log.go
+++ server/repository/log.go
@@ -13,7 +13,7 @@ type Log struct {
 	ServerID  uuid.UUID `gorm:"type:char(36);not null"`
 	Server    Server    `gorm:"foreignkey:ServerID;references:ID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE"`
 	StdOut    string    `gorm:"type:text;not null"`
-	CreatedAt time.Time `gorm:"type:DATETIME;not null;default:CURRENT_TIMESTAMP"`
+	CreatedAt time.Time
 }
 
 func (Log) TableName() string {
