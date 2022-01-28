f208720278f19d80315eab74247160841e11e81c
logica <84759675+logica0419@users.noreply.github.com>
2022-01-29 05:25:33 +0900

    added sleep to dummy command

--- server/benchmark/command.go
+++ server/benchmark/command.go
@@ -1,6 +1,6 @@
 package benchmark
 
 var commands = map[string]string{
-	"isucon-test":      "echo %s",
+	"isucon-test":      "sleep 1m; echo %s",
 	"isucon11-qualify": "cd bench && ./bench -all-addresses 127.0.0.11 -target %s -tls -jia-service-url http://127.0.0.1:4999",
 }
