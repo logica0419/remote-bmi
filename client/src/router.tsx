76703faa4e82702211e97dcf50db8a6b7a188f00
logica <84759675+logica0419@users.noreply.github.com>
2022-08-25 23:55:59 +0900

    fixed type error

--- client/src/router.tsx
+++ client/src/router.tsx
@@ -1,12 +1,21 @@
 import { useEffect, VFC } from "react";
 import { Route, Routes, useNavigate } from "react-router-dom";
-import Home from "./components/Home";
 import Auth from "./components/Auth";
+import Home from "./components/Home";
 import { useLoginCheck } from "./utils/login";
+
+const RootNavigator: VFC = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    navigate("/");
+  }, []);
+
+  return <></>;
+};
 
 const Router: VFC = () => {
   const { authorized, isFetching, fetchLoginStatus } = useLoginCheck();
-  const navigate = useNavigate();
 
   useEffect(() => {
     fetchLoginStatus();
@@ -17,22 +26,12 @@ const Router: VFC = () => {
       {authorized ? (
         <>
           <Route path="/" element={<Home />} />
-          <Route
-            path="/*"
-            element={() => {
-              navigate("/");
-            }}
-          />
+          <Route path="/*" element={<RootNavigator />} />
         </>
       ) : isFetching ? (
         <>
           <Route path="/" element={<h2>Checking User Status...</h2>} />
-          <Route
-            path="/*"
-            element={() => {
-              navigate("/");
-            }}
-          />
+          <Route path="/*" element={<RootNavigator />} />
         </>
       ) : (
         <>
@@ -40,12 +39,7 @@ const Router: VFC = () => {
             path="/"
             element={<Auth fetchLoginStatus={fetchLoginStatus} />}
           />
-          <Route
-            path="/*"
-            element={() => {
-              navigate("/");
-            }}
-          />
+          <Route path="/*" element={<RootNavigator />} />
         </>
       )}
     </Routes>
