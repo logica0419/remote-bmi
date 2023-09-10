ef279c132d448cff20468fd39fd1f3288a9376b1
logica <84759675+logica0419@users.noreply.github.com>
2023-09-11 02:18:30 +0900

    Update command.go

--- server/benchmark/command.go
+++ server/benchmark/command.go
@@ -17,7 +17,7 @@ var commands = map[string]command{
 		workDir: ".",
 		createCmd: func(benchServerIP string, servers []*repository.Server, serverNumber int) (string, error) {
 			return "sleep 2s", nil
-		}
+		},
 	},
 
 	"isucon11-qualify": {
